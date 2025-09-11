dd7173778034e094a3aaa07c4bd0781fc4d06c48
Kanagat01 <skanagat101@gmail.com>
2025-09-11 21:40:06 +0500

    hotfix back-btn

--- frontend/src/widgets/BackBtnContainer.tsx
+++ frontend/src/widgets/BackBtnContainer.tsx
@@ -1,17 +1,11 @@
 import { BsArrowLeft } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
-import Routes from "~/shared/routes";
 
 export function BackBtnContainer({ grayBg = true }: { grayBg?: boolean }) {
   const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate(Routes.CATALOG_3D, { replace: true });
-  };
-
   return (
     <div className={`back-btn-container${grayBg ? " gray-bg" : ""}`}>
-      <button onClick={handleClick}>
+      <button onClick={() => navigate(-1)}>
         <BsArrowLeft />
         Назад
       </button>
